95276c29259515f805c8c4a1f2d72a8037817909
Theskrtnerd <tvbbd2@gmail.com>
2023-10-22 17:20:27 +1030

    initial commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,15 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontSize: {
+			sm: '0.8rem',
+			base: '1rem',
+			xl: '1.333rem',
+			'2xl': '1.777rem',
+			'3xl': '2.369rem',
+			'4xl': '3.157rem',
+			'5xl': '4.209rem',
+		},
 		extend: {
 			colors: {
 				pri: {
