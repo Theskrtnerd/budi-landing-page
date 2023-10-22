33645bd50b29803de947d6180b5ca31083f13820
Theskrtnerd <tvbbd2@gmail.com>
2023-10-22 17:09:49 +1030

    initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				pri: {
+				  100: '#7423ae',
+				  80: '#ae56f6',
+				  60: '#ead5ff',
+				},
+				sec: {
+					100: '#f2ca45',
+					80: '#f7e290',
+					60: '#fbf1ca',
+				}
+			},
+			fontFamily: {
+				'sans': ['"Montserrat"', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	plugins: [],
+}
