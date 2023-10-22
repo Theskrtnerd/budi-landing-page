df023fa740a37c60eb91f78bdcc3882509e0b6b2
khanhgn <127292124+khanhgn@users.noreply.github.com>
2023-10-23 09:46:01 +1030

    added icon assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,10 +1,18 @@
 import logo from "./logo.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
+import freeShipping from "./free-shipping-xxl.png";
+import discounts from "./offer.png";
+import carbonNeutral from "./green-house.png";
+
+
 
 
 export {
   logo,
   menu,
   close,
+  freeShipping,
+  discounts,
+  carbonNeutral
 };
