b998b936d450330106a25bdb032e74fd4b483c5a
Theskrtnerd <tvbbd2@gmail.com>
2023-10-23 02:57:15 +1030

    added userCount

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,11 +6,11 @@ export default {
 		fontSize: {
 			sm: '0.8rem',
 			base: '1rem',
-			xl: '1.333rem',
-			'2xl': '1.777rem',
-			'3xl': '2.369rem',
-			'4xl': '3.157rem',
-			'5xl': '4.209rem',
+			xl: '1.25rem',
+			'2xl': '1.563rem',
+			'3xl': '1.953rem',
+			'4xl': '2.441rem',
+			'5xl': '3.052rem',
 		},
 		extend: {
 			colors: {
