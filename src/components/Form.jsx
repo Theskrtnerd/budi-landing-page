c8d17fea10186dade202f20a62bd96dc07f6ee74
khanhgn <127292124+khanhgn@users.noreply.github.com>
2023-10-24 11:03:29 +1030

    initial

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,32 +1,78 @@
-// BasicExampleForm.js
-// Create a fully operational form.
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Form, {
-  Input,
-  Select,
-  FormButton,
-} from 'react-form-component'
+import React, { useState } from 'react';
 
-const BasicExampleForm = () =>
-  <Form fields={['name', 'email', 'type']}>
-    <Input
-      name='name'
-      label='User name'
-    />
-    <Input
-      name='email'
-      type='email'
-      label='E-mail'
-    />
-    <Select
-      name='type'
-      label='Type of a user'
-      options={['Viewer', 'Moderator', 'Admin']}
-    />
-    <FormButton
-      onClick={fields => console.log(fields)}
-    >Save</FormButton>
-  </Form>
+const encode = (data) => {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
+};
 
-export default BasicExampleForm
+function ContactForm() {
+  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', ...formData }),
+    })
+      .then(() => alert('Success!'))
+      .catch((error) => alert(error));
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <form className="max-w-md w-full" onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label htmlFor="name" className="block text-gray-700 text-sm font-medium">
+            Your Name:
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            className="form-input"
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-gray-700 text-sm font-medium">
+            Your Email:
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            className="form-input"
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="message" className="block text-gray-700 text-sm font-medium">
+            Message:
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            className="form-textarea"
+          />
+        </div>
+        <div className="mb-4">
+          <button type="submit" className="btn-primary">
+            Send
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default ContactForm;
