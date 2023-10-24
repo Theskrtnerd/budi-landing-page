c83b8bce1f9b88cb825bfbf506f76084e663133d
khanhgn <127292124+khanhgn@users.noreply.github.com>
2023-10-24 17:14:04 +1030

    yes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { motion } from 'framer-motion'
 import { styles } from '../style'
 import { ComputersCanvas } from './canvas'
+import StarsCanvas from './canvas'
 
 
 const Hero = () => {
@@ -22,23 +23,8 @@ const Hero = () => {
         <ComputersCanvas />
       </div>
       
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='footer'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
-            />
-          </div>
-        </a>
-      </div>
+      
+
     </section>
   )
 }
